6c854f28bbb187b3a52fd30018fa297afb8f1ff2
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-11-29 16:12:27 +0100

    JS --> complet PHASE 0

--- script.js
+++ script.js
@@ -17,6 +17,25 @@ Ses envies :
 */
 
 /*
+PHASE 0 : activer le bouton pour lancer le jeu
+1) Cliquer sur le bouton du milieu
+2) Ajouter un compteur qui attend d'avoir une valeur max de 5 
+3) On lance la fonction startTama
+*/
+const start = () => {
+// 1) Cliquer sur le bouton du milieu
+const buttonCenter = document.querySelector('.js-button[data-direction="center"]');
+// 2) Ajouter un compteur qui attend d'avoir une valeur max de 5
+let clickCounter = 0;
+buttonCenter.addEventListener("click", () => {
+    clickCounter++;
+    // 3) On lance la fonction startTama
+    if (clickCounter === 5) {
+        birth();
+    }
+});
+};
+/*
 PHASE 1 : la naissance de mon tama
 1) demander le nom de mon personnage
 2) fait éclore mon oeuf pour passer au poussin
@@ -25,23 +44,26 @@ PHASE 1 : la naissance de mon tama
 5) mettre les scores des vitals à 5
 */
 
-// 1) demander le nom de mon personnage 
-const tamaName = prompt("Quel nom a votre TamaStudi?");
-console.log(tamaName); 
-// 2) fait éclore mon oeuf pour passer au poussin
-const character = document.querySelector(".js-character");
-character.textContent = "🐣";
-// 3) affiche mes vitals
-const vitals = document.querySelector(".js-vitals");
-vitals.classList.remove("hidden"); 
-// 4) affiche le nom de mon tama dans les vitals
-const nameDisplay = document.querySelector(".js-tamaName");
-nameDisplay.textContent = tamaName;
-// 5) mettre les scores des vitals à 5
-const scoresDisplay = document.querySelectorAll(".js-score");
-scoresDisplay.forEach((score) => {
-    score.textContent = "5";
-});
+const birth = () => {
+    console.log("startTama");
+    // 1) demander le nom de mon personnage 
+    const tamaName = prompt("Quel nom a votre TamaStudi?");
+    console.log(tamaName); 
+    // 2) fait éclore mon oeuf pour passer au poussin
+    const character = document.querySelector(".js-character");
+    character.textContent = "🐣";
+    // 3) affiche mes vitals
+    const vitals = document.querySelector(".js-vitals");
+    vitals.classList.remove("hidden"); 
+    // 4) affiche le nom de mon tama dans les vitals
+    const nameDisplay = document.querySelector(".js-tamaName");
+    nameDisplay.textContent = tamaName;
+    // 5) mettre les scores des vitals à 5
+    const scoresDisplay = document.querySelectorAll(".js-score");
+    scoresDisplay.forEach((score) => {
+        score.textContent = "5";
+    });
+}
 
-
-
+// Lance la fonction de "début de mon Tama"
+start();
